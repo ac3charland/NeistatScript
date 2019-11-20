c72eac8e53a2e1940c43275b846765b02f8b3495
Alex Charland <ac3charland@gmail.com>
2019-11-20 08:01:38 -0600

    Download video from URL to video directory from anywhere on computer. Automatically create folder for video.

---
+++ run.sh
@@ -0,0 +1,7 @@
+cd ~/Documents/Other/VlogCatalog/
+FOLDERCOUNT=$(ls -l | grep -c ^d)
+NEWFOLDERNUM=$((FOLDERCOUNT+1))
+NEWFOLDERNAME="VLOG_00${NEWFOLDERNUM}"
+mkdir ${NEWFOLDERNAME}
+cd ${NEWFOLDERNAME}
+youtube-dl $1
